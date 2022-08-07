08e2899ece2363675a9c82e50b79bbe80367cb6d
Nathan Wang <nathan.r.wang@gmail.com>
2022-08-06 19:01:59 -0700

    leaderboard progress bar

--- components/Leaderboard.js
+++ components/Leaderboard.js
@@ -6,19 +6,21 @@ export default function Leaderboard() {
   const sortedPeople = useMemo(() => {
     return people.sort((a, b) => {
       return b.noiseScore - a.noiseScore;
-    })
+    });
   }, [people]);
   const yourPlace = people.findIndex(
     (p) => p.uid === auth.currentUser.uid
   ) + 1;
+  const maxScore = sortedPeople[0].noiseScore;
 
   return (
-    <div className="w-full sm:w-1/2 px-4">
-      <h1 className="text-center text-3xl font-bold">Leaderboard</h1>
-      <ul role="list" className="divide-y divide-gray-200">
+    <div className="w-full sm:w-1/2">
+      <h1 className="text-center text-3xl font-bold mb-4">Leaderboard</h1>
+      <ul role="list" className="">
         {sortedPeople.map((person) => (
-          <li key={person.uid} className="py-4">
-            <div className="flex space-x-3">
+          <li key={person.uid} className="p-4 relative">
+            <div className="absolute inset-y-0 left-0 bg-blue-200" style={{ width: `${person.noiseScore/maxScore*100}%` }} />
+            <div className="flex space-x-3 relative">
               <div className="flex-1 space-y-1">
                 <div className="flex items-center justify-between">
                   <h3 className="text-sm font-medium">{person.displayName}</h3>
